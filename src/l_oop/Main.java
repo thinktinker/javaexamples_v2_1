ed0a00caacfe56e4e06a428bf809ece12835c161
Martin Leong <martinleongmo@gmail.com>
2026-05-06 08:07:00 +0800

    Modify beverage instances and add food item
    Updated beverage names and added a food item.

--- src/l_oop/Main.java
+++ src/l_oop/Main.java
@@ -5,14 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Creating instances of the Beverage class (object)
-        Beverage beverage1 = new Beverage("Hot Americano", 5, false);
-        Beverage beverage2 = new Beverage("Ice Coffee", 4, true);
+        Beverage beverage1 = new Beverage("Cappucino", 5, false);
+        Beverage beverage2 = new Beverage("Expresso", 3, true);
 
         System.out.println(beverage1.toString());
         System.out.println("Please pay: $" + beverage1.calculateTotalPrice());
 
         System.out.println(beverage2.toString());
         System.out.println("Please pay: $" + beverage2.calculateTotalPrice());
+
+        Food foodItem1 = new Food("Hawaiian Pizza", 9.90, false);
+        System.out.println(foodItem1.toString());
+
 
     }
 }
