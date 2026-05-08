df2f58cb05214b53e74acbe5b2d12cb9f6cf4389
Martin Leong <martinleongmo@gmail.com>
2026-05-08 08:55:19 +0800

    Add Calculator class with basic arithmetic methods

---
+++ src/l_unit_test/model/Calculator.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Calculator {
+
+    // add
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    // subtraction
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    // multiplication
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+    // division
+    public double division(double a, double b){
+        return a / b;
+    }
+
+}
