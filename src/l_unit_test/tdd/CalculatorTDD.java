15ca9f141daaa2b0beb1f1c27c6ef8f0fc900d87
Martin Leong <martinleongmo@gmail.com>
2026-05-10 19:26:40 +0800

    Add basic calculator functionality in CalculatorTDD
    Implemented basic arithmetic operations in CalculatorTDD.

---
+++ src/l_unit_test/tdd/CalculatorTDD.java
@@ -0,0 +1,31 @@
+package l_unit_test.tdd;
+
+public class CalculatorTDD {
+
+    // Create the skeletal code on the functional parts of a calculator program
+    // addition
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    // subtraction
+    public double subtract(double a, double b){
+        return a - b;
+    }
+
+    // multiplication
+    public double multiply(double a, double b){
+        return a * b;
+    }
+
+    // division
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    // modulus
+    public double modulus(double a, double b){
+        return a % b;
+    }
+
+}
